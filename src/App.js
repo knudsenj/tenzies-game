4b38601344846b6cecafd14ea76592df194b16ae
Joel Knudsen <knudsen.joel@gmail.com>
2021-12-05 15:30:58 -0800

    Add ability to hold dice.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import Die from "./components/Die";
 import { useState } from  "react";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [dice, setDice] = useState(generateNewDice())
+  const [dice, setDice] = useState(generateAllNewDice())
 
-  function generateNewDice() {
-    return Array(10).fill(0, 0, 10).map(() => Math.floor(Math.random() * 6 + 1));
+  function generateNewDie() {
+    return Math.floor(Math.random() * 6 + 1)
+  }
+
+  function generateAllNewDice() {
+    return Array(10).fill(0, 0, 10).map(() => ({
+      value: generateNewDie(),
+      isHeld: false,
+      id: nanoid()
+    }));
   }
 
   function rollDice() {
-    setDice(generateNewDice());
+    setDice(prevDice => prevDice.map(die => die.isHeld ? die : {...die, value: generateNewDie()}));
+  }
+
+  function toggleHold(id) {
+    setDice(prevDice => prevDice.map(die => die.id === id ? {...die, isHeld: !die.isHeld} : die))
   }
 
   return (
     <main>
       <div className="dice-container">
-        {dice.map((die, idx) => <Die key={idx} value={die} />)}
+        {dice.map((die, idx) => (
+          <Die 
+            key={die.id} 
+            id={die.id}
+            value={die.value} 
+            isHeld={die.isHeld} 
+            toggleHold={toggleHold}
+          />
+        ))}
       </div>
 
       <button className="roll-button" onClick={rollDice}>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,12 @@
 export default function Die(props) {
   const value = Math.min(Math.max(props.value, 1), 6);
 
+  const style = {
+    background: props.isHeld ? "#59E391" : "#FFFFFF"
+  }
+
   return (
-    <div className="die">
+    <div className="die" style={style} onClick={() => props.toggleHold(props.id)}>
       {value}
     </div>
   );
