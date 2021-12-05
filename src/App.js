83527daf006084b30c1d726e13f654f2228196ff
Joel Knudsen <knudsen.joel@gmail.com>
2021-12-05 15:43:09 -0800

    Add instructions and title.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,11 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+      </p>
+
       <div className="dice-container">
         {dice.map((die, idx) => (
           <Die 
