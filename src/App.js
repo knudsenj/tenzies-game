d5e82a41bd925c17898c1063ff84f82fdbd5d23e
Joel Knudsen <knudsen.joel@gmail.com>
2021-12-05 16:16:37 -0800

    Add win state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Die from "./components/Die";
-import { useState } from  "react";
-import { nanoid } from "nanoid";
+import Die from "./components/Die"
+import { useState, useEffect } from  "react"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice())
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    // win if all dies are held and all dies are the same number
+    const victory = dice.slice(1).reduce(
+      (heldNumber, die) => die.isHeld && heldNumber === die.value && die.value, 
+      dice[0].isHeld && dice[0].value
+    );
+
+    setTenzies(victory);
+  }, [dice])
 
   function generateNewDie() {
+    // generate number between 1 and 6, inclusive
     return Math.floor(Math.random() * 6 + 1)
   }
 
@@ -18,7 +31,11 @@ function App() {
   }
 
   function rollDice() {
-    setDice(prevDice => prevDice.map(die => die.isHeld ? die : {...die, value: generateNewDie()}));
+    if (tenzies) {
+      setDice(generateAllNewDice())
+    } else {
+      setDice(prevDice => prevDice.map(die => die.isHeld ? die : {...die, value: generateNewDie()}));
+    }
   }
 
   function toggleHold(id) {
@@ -27,6 +44,7 @@ function App() {
 
   return (
     <main>
+      { tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
@@ -45,7 +63,7 @@ function App() {
       </div>
 
       <button className="roll-button" onClick={rollDice}>
-        Roll Dice
+        { tenzies ? "New Game" : "Roll Dice" }
       </button>
     </main>
   );
